dbeb9e56e4ed2747496ec0cd9b76a2c431629f7b
João Vitor M <joaomatosf240@gmail.com>
2025-03-23 15:52:37 -0300

    Update Introducao.py

--- Classes/Introducao.py
+++ Classes/Introducao.py
@@ -5,7 +5,22 @@ class Books:
         self.year = pub_year
         self.Id_Livro = ID
         self.checked_out = False
+     def checkout(self):
+        
+        if not self.checked_out: #SE a saida do livro não esta True:
+            self.checked_out = True
+            print(f'O livro "{self.title}" foi emprestado.')
+        else:
+            print(f'O livro "{self.title}" já está emprestado.')
 
+    def return_book(self):
+        
+        if self.checked_out: #SE a saida do livro está True
+            self.checked_out = False
+            print(f'O livro "{self.title}" foi devolvido.')
+        else:
+            print(f'O livro "{self.title}" já estava na biblioteca.')
+            
 # Criando objetos
 book1 = Books("Romeu e Julieta", "William Shakespeare", 1597, "SHA-1597")
 book2 = Books("Carros", "Autor Desconhecido", 2006, "CAR-2006")
